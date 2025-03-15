6b485a791250c5feeae142554a9edc271c33f48a
ahmedgold1999 <82882181+ahmedgold1999@users.noreply.github.com>
2025-03-15 21:34:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -103,34 +103,16 @@ const startTimer = (time) => {
 };
 
 const loadingAnimation = () => {
-  startBtn.innerHTML = "Loading";
+  startBtn.innerHTML = "جاري جلب الاسئلة";
   const loadingInterval = setInterval(() => {
     if (startBtn.innerHTML.length === 10) {
-      startBtn.innerHTML = "Loading";
+      startBtn.innerHTML = "جاري جلب الاسئلة";
     } else {
       startBtn.innerHTML += ".";
     }
   }, 500);
 };
-function defineProperty() {
-  var osccred = document.createElement("div");
-  osccred.innerHTML =
-    "A Project By <a href='https://www.youtube.com/@opensourcecoding' target=_blank>Open Source Coding</a>";
-  osccred.style.position = "absolute";
-  osccred.style.bottom = "0";
-  osccred.style.right = "0";
-  osccred.style.fontSize = "10px";
-  osccred.style.color = "#ccc";
-  osccred.style.fontFamily = "sans-serif";
-  osccred.style.padding = "5px";
-  osccred.style.background = "#fff";
-  osccred.style.borderTopLeftRadius = "5px";
-  osccred.style.borderBottomRightRadius = "5px";
-  osccred.style.boxShadow = "0 0 5px #ccc";
-  document.body.appendChild(osccred);
-}
 
-defineProperty();
 
 const submitBtn = document.querySelector(".submit"),
   nextBtn = document.querySelector(".next");
